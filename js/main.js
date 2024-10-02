3d2493b8be157c7f35e3f0ae05acf3bf6ebf818d
ashlynguinnformayor <ashlynguinnformayor@gmail.com>
2024-10-02 12:46:24 -0500

    restyling and new features

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 console.log("main");
+
+let ashlyn_promise=document.getElementById('ashlyn_promise');
+ashlyn_promise.addEventListener("click",function(){
+  ashlyn_promise.style.visibility="hidden";
+})
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("ashlyn_promise").style.visibility = "visible";
+  } else {
+    document.getElementById("ashlyn_promise").style.visibility = "hidden";
+  }
+  prevScrollpos = currentScrollPos;
+}
---
+++ js/main_menu.js
@@ -0,0 +1,10 @@
+
+const menu=`
+<br><br>
+<a href="index.html">HOME</a>
+<a href="about.html">ABOUT ASHLYN</a>
+<a href="support.html">SUPPORT ASHLYN</a>
+`;
+
+const main_menu=document.getElementById('main_menu');
+main_menu.innerHTML=menu;
