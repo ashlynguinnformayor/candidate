d3c3253f274cb5e57852fe6552d1d5ecd5a02ef2
ashlynguinnformayor <ashlynguinnformayor@gmail.com>
2024-10-04 21:07:59 -0500

    menu corners

--- js/main_menu.js
+++ js/main_menu.js
@@ -1,9 +1,11 @@
 
 const menu=`
 <br><br>
+<a class="endpoint-left">&nbsp;</a>
 <a href="index.html">HOME</a>
 <a href="about.html">ABOUT ASHLYN</a>
 <a href="support.html">SUPPORT ASHLYN</a>
+<a class="endpoint-right">&nbsp;</a>
 `;
 
 const main_menu=document.getElementById('main_menu');
