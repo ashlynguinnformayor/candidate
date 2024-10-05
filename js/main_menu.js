b1c56f8a0cceaf189bfd46be03936686fb59a719
ashlynguinnformayor <ashlynguinnformayor@gmail.com>
2024-10-04 21:39:57 -0500

    menu change

--- js/main_menu.js
+++ js/main_menu.js
@@ -1,11 +1,13 @@
 
 const menu=`
 <br><br>
-<a class="endpoint-left">&nbsp;</a>
-<a href="index.html">HOME</a>
-<a href="about.html">ABOUT ASHLYN</a>
-<a href="support.html">SUPPORT ASHLYN</a>
-<a class="endpoint-right">&nbsp;</a>
+<ul>
+<li><a>&nbsp;</a></li>
+<li><a href="index.html">HOME</a></li>
+<li><a href="about.html">ABOUT ASHLYN</a></li>
+<li><a href="support.html">SUPPORT ASHLYN</a></li>
+<li><a>&nbsp;</a></li>
+</ul>
 `;
 
 const main_menu=document.getElementById('main_menu');
